5e363912cc45138d3bd47fcada0d45cbb2527d5e
unknown <coffebealzarethes@gmail.com>
2025-05-22 08:31:41 -0600

    Controllers added

---
+++ GestorDeTareasMelbar/Controllers/IntegranteController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestorDeTareasMelbar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IntegranteController : ControllerBase
+    {
+    }
+}
--- GestorDeTareasMelbar/obj/Debug/net8.0/GestorDeTareasMelbar.AssemblyInfo.cs
+++ GestorDeTareasMelbar/obj/Debug/net8.0/GestorDeTareasMelbar.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestorDeTareasMelbar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+988469217fd7d68c9ac8233b118352581c524c65")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestorDeTareasMelbar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestorDeTareasMelbar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
